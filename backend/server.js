d11551a1319fcdc149928c79ec71944496b1e8c8
Rohit Mehra <rohitkumar23092007@gmail.com>
2026-08-19 23:46:01 +0530

    sql

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,17 @@
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
+
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "food_delivery"
+});
